3411b794a3233f5ba440d44e643e74c15f13166a
epicodus <student@epicodus.com>
2014-08-20 13:41:22 -0700

    add train in user interface

--- train_interface.rb
+++ train_interface.rb
@@ -54,6 +54,43 @@ def system_menu
     end
   end
 end
+
+def passenger_menu
+  loop do
+    puts "\n\n ******* PASSENGER MENU ******** \n\n"
+    puts "Press ['l'] to list all trains lines in the system_menu"
+    puts "Press ['v'] to view all the stations in the system"
+    puts "Press ['stops'] to see all the stops in a train line"
+    puts "Press ['lines'] to see all the train lines going to a station"
+    puts "Press ['m'] to go back to main menu"
+    menu_choice = gets.chomp.downcase
+    if menu_choice == 'l'
+      list_trains
+    elsif menu_choice == 'v'
+      list_stations
+    elsif menu_choice == 'stops'
+      list_stops
+    elsif menu_choice == 'lines'
+      list_lines
+    elsif menu_choice == 'm'
+      main_menu
+    else
+      puts "That is not a valid option."
+    end
+  end
+end
+
+def add_train
+  puts "What is the name of the train you would like to add?"
+  train_name = gets.chomp
+  new_train = Train.new({:name => train_name})
+  new_train.save
+  binding.pry
+  puts "New Train Created!"
+  main_menu
+end
+
 main_menu
+
 
 
